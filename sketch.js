f35254de521c83d8abbe6bb3f7119bae4a98d11f
shoccho <sfb.shoccho@gmail.com>
2020-08-25 20:42:56 +0600

    after a lot of struggle to swap just a single tile switching to 2d array

--- sketch.js
+++ sketch.js
@@ -1,18 +1,42 @@
 var blocks = [];
 var scl = 100;
+var emp;
 function setup() {
   createCanvas(300, 300);
-  for (var i = 0; i < 9; i++) {
-    blocks.push(new Tile(i % 3, floor(i / 3), i));
-  }
-  //   blocks.push();
-}
+  //   for (var i = 0; i < 9; i++) {
+  //     blocks.push(new Tile(i % 3, floor(i / 3), i));
+  //   }
+  emp = new Tile(0, 2, 8);
+  var blocks1 = [];
+  var blocks2 = [];
+  var blocks3 = [];
 
-function draw() {
-  background("#145374");
-  for (var i = 0; i < 9; i++) {
-    blocks[i].show();
+  blocks1.push(new Tile(0, 0, 3));
+  blocks1.push(new Tile(1, 0, 0));
+  blocks1.push(new Tile(2, 0, 1));
+  blocks2.push(new Tile(0, 1, 4));
+  blocks2.push(new Tile(1, 1, 5));
+  blocks2.push(new Tile(2, 1, 2));
+  blocks3.push(emp);
+  blocks3.push(new Tile(1, 2, 6));
+  blocks3.push(new Tile(2, 2, 7));
+  blocks.push(blocks1);
+  blocks.push(blocks2);
+  blocks.push(blocks3);
+  displayblocks(blocks);
+}
+function displayblocks(p) {
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      p[i][j].show();
+    }
   }
+}
+function draw() {
+  //   background("#145374");
+  //   displayblocks(blocks);
+  //   noLoop();
+  document.getElementById("wadup").innerHTML = "value= " + genValue(blocks);
 }
 function mouseClicked() {
   console.log(genValue(blocks));
@@ -32,12 +56,4 @@ class Tile {
     if (this.v != 8)
       text(this.v + 1, this.x * scl + scl / 3, this.y * scl + scl / 3, scl);
   }
-}
-
-function genValue(blocks) {
-  var val = 0;
-  for (var i = 0; i < 9; i++) {
-    val += abs(blocks[i].v - i);
-  }
-  return val;
 }
